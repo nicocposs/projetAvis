a3dda4d5b54200f2e32957385b51b460ca727679
nicocposs <msnjus@hotmail.fr>
2022-07-07 10:52:40 +0200

    Projet créé

---
+++ src/main/java/fr/orsys/projetavis/ProjetavisApplication.java
@@ -0,0 +1,13 @@
+package fr.orsys.projetavis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetavisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetavisApplication.class, args);
+	}
+
+}
